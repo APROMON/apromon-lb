b79719cdf164b69993eaf10b9d6d0f1baf81bcb4
APROMON <132260083+APROMON@users.noreply.github.com>
2023-06-04 11:31:33 +0300

    Add files via upload

---
+++ servo_coordinates_car_area.ino
@@ -0,0 +1,59 @@
+#include <Pixy2.h>
+#include <Servo.h>
+#include <SPI.h>
+Pixy2 pixy;
+int in2 = 3;
+int in1 = 4;
+const int trigPin = 12;
+const int echoPin = 13;
+int servoPin = 11;
+Servo Servo1;
+
+void setup() {
+  
+  Serial.begin(9600);
+  pixy.init();
+  Servo1.attach(servoPin);
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in1, OUTPUT);
+}
+
+void loop() {
+   int blocks;
+  pixy.ccc.getBlocks();
+  Servo1.write(90);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+ if (pixy.ccc.numBlocks) {
+    for (int i = 0; i < pixy.ccc.numBlocks; i++) {
+      int blockX = pixy.ccc.blocks[i].m_x;
+      int blockY = pixy.ccc.blocks[i].m_y;
+      int blockWidth = pixy.ccc.blocks[i].m_width;
+      int blockHeight = pixy.ccc.blocks[i].m_height;
+      int area = blockWidth * blockHeight;
+      uint16_t signature = pixy.ccc.blocks[i].m_signature;
+      int steeringAngle = map(blockX, 0, 316, 65, 125);
+      String signatureName = String(signature);
+      Servo1.write(steeringAngle);
+
+      Serial.print("Detected object (");
+      Serial.print(signatureName);
+      Serial.print(") at (");
+      Serial.print(blockX);
+      Serial.print(", ");
+      Serial.print(blockY);
+      Serial.print("), ");
+      Serial.print("Area: ");
+      Serial.println(area);
+
+      if (area < 15000) {
+        // Disactivate the DC
+          digitalWrite(in1, HIGH);
+          digitalWrite(in2, HIGH);
+     
+   }
+  }
+ }
+}
