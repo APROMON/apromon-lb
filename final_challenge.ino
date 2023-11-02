6cf4b68c96fb12e84254f29dab2a52d8db240d40
APROMON <132260083+APROMON@users.noreply.github.com>
2023-11-02 17:28:18 +0200

    final_challenge.ino

--- final_challenge.ino
+++ final_challenge.ino
@@ -8,9 +8,9 @@ const int leftTrigPin = 38;
 const int leftEchoPin = 39;
 const int rightTrigPin = 36;
 const int rightEchoPin = 37;
-const int frontTrigPin = 34;
-const int frontEchoPin = 35;
-const int servoPin = 11;
+const int frontTrigPin = 31;
+const int frontEchoPin = 30;
+const int servoPin = 41;
 const int buttonPin = 26;
 const int in2 = 3;
 const int in1 = 4;
@@ -51,23 +51,37 @@ void setMotorSpeed(int speed) {
 }
 
 void followBlock(int signature) {
-  int servoAngle = 90;
+  int servoAngle = 130;
+  int difference = leftDistance - rightDistance;
+  setMotorSpeed(130);
 
   if (signature == 1) {
-    if (topRightX <= 20) {
-      servoAngle = 90;
+    if (difference < 6 && difference > -6) {
+      servoAngle = 130;
+    } else if (rightDistance < 10) {
+      servoAngle = 80;
+    } else if (leftDistance < 10) {
+      servoAngle = 170;
+    } else if (topRightX <= 60) {
+      servoAngle = 130;
     } else if (topRightX <= 140) {
-      servoAngle = map(topRightX, 20, 140, 100, 125);
+      servoAngle = map(topRightX, 60, 140, 140, 170);
     } else {
-      servoAngle = 125;
+      servoAngle = 170;
     }
   } else {
-    if (topLeftX >= 300) {
-      servoAngle = 90;
-    } else if (topLeftX >= 180) {
-      servoAngle = map(topLeftX, 180, 300, 55, 80);
+    if (difference < 6 && difference > -6) {
+      servoAngle = 130;
+    } else if (rightDistance < 10) {
+      servoAngle = 80;
+    } else if (leftDistance < 10) {
+      servoAngle = 170;
+    } else if (topLeftX >= 260) {
+      servoAngle = 130;
+    } else if (topLeftX >= 170) {
+      servoAngle = map(topLeftX, 170, 260, 80, 120);
     } else {
-      servoAngle = 55;
+      servoAngle = 80;
     }
   }
   servo.write(servoAngle);
@@ -78,19 +92,19 @@ void followCenter(int signature) {
 
   if (signature == 1) {
     if (leftDistance < 8) {
-      servoAngle = 125;
+      servoAngle = 170;
     } else if (rightDistance < 8) {
-      servoAngle = 55;
+      servoAngle = 80;
     } else {
-      servoAngle = map(redCenter, 0, 330, 55, 125);
+      servoAngle = map(redCenter, 0, 330, 80, 170);
     }
   } else {
     if (leftDistance < 8) {
-      servoAngle = 125;
+      servoAngle = 170;
     } else if (rightDistance < 8) {
-      servoAngle = 55;
+      servoAngle = 80;
     } else {
-      servoAngle = map(greenCenter, 0, 330, 55, 125);
+      servoAngle = map(greenCenter, 0, 330, 80, 170);
     }
   }
   servo.write(servoAngle);
@@ -135,7 +149,7 @@ void loop() {
   Serial.print(rightDistance);
   Serial.print(" cm, Difference: ");
   Serial.print(difference);
-  Serial.print(" cm, Front Distance; ");
+  Serial.print(" cm, Front Distance: ");
   Serial.print(frontDistance);
   Serial.print('\n');
 
@@ -146,13 +160,21 @@ void loop() {
   int redArea = 0;
   int greenArea = 0;
   int lowSpeed = 0;
+  int redHeight = 0;
+  int greenHeight = 0;
+  int redBlockHeight = 0;
+  int greenBlockHeight = 0;
+//  int servoSteering;
+  int servoAngle = 0;
+//  int mappedValue = 0;
+
 
 
   for (int i = 0; i < numBlocks; i++) {
     int blockSignature = pixy.ccc.blocks[i].m_signature;
     int blockHeight = pixy.ccc.blocks[i].m_y + pixy.ccc.blocks[i].m_height;
     
-    if (blockSignature == 2 && blockHeight > 100) {
+    if (blockSignature == 2) {
       lowSpeed = 1;
       unsigned long currentOrangeBlockTime = millis();
       int orangeDelay = currentOrangeBlockTime - previousOrangeBlockTime;
@@ -160,7 +182,7 @@ void loop() {
         orangeCount++;
         previousOrangeBlockTime = currentOrangeBlockTime;
       }
-    } else if (blockSignature == 6 && blockHeight > 100) {
+    } else if (blockSignature == 6) {
       lowSpeed = 1;
       unsigned long currentBlueBlockTime = millis();
       int blueDelay = currentBlueBlockTime - previousBlueBlockTime;
@@ -168,22 +190,38 @@ void loop() {
         blueCount++;
         previousBlueBlockTime = currentBlueBlockTime;
       }
-    } else if (blockSignature == 1 && blockHeight > 100) {
+    } else if (blockSignature == 1 && blockHeight > 50 ) {
       redArea = pixy.ccc.blocks[i].m_width * pixy.ccc.blocks[i].m_height;
       redCenter = pixy.ccc.blocks[i].m_x + pixy.ccc.blocks[i].m_width / 2;
       topRightX = pixy.ccc.blocks[i].m_x + pixy.ccc.blocks[i].m_width;
+      redHeight = pixy.ccc.blocks[i].m_y + pixy.ccc.blocks[i].m_height;
       redGreenCount++;
-    } else if (blockSignature == 4 && blockHeight > 100) {
+      if (redBlockHeight > 80 && redCenter > 130 && redCenter < 190) {
+        servo.write(100);
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+        setMotorSpeed(130);
+        delay(800);
+      } 
+    } else if (blockSignature == 4 && blockHeight > 50) {
       greenArea = pixy.ccc.blocks[i].m_width * pixy.ccc.blocks[i].m_height;
       greenCenter = pixy.ccc.blocks[i].m_x + pixy.ccc.blocks[i].m_width / 2;
       topLeftX = pixy.ccc.blocks[i].m_x;
+      greenHeight = pixy.ccc.blocks[i].m_y + pixy.ccc.blocks[i].m_height;
       redGreenCount++;
+      if (greenBlockHeight > 80 && greenCenter > 130 && greenCenter < 190) {
+        servo.write(100);
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+        setMotorSpeed(130);
+        delay(800);
+      } 
     }
   }
 
   if (orangeCount >= 12 || blueCount >= 12) {
     if (stopTime == 0) {
-      stopTime = millis() + 2500;
+      stopTime = millis() + 1500;
     } else {
       if (millis() >= stopTime) {
         digitalWrite(in1, LOW);
@@ -195,25 +233,24 @@ void loop() {
   }
 
   if (redGreenCount > 0) {
-    setMotorSpeed(100);
-    if (redArea > greenArea) {
-      if (redArea > 2000) {
-        setMotorSpeed(150);
+    if (redHeight > greenHeight) {
+      if (redHeight > 100) {
+//        setMotorSpeed(120);
         followBlock(1);
         direction = 1;
       } else {
         followCenter(1);
       }
     } else {
-      if (greenArea > 2000) {
-        setMotorSpeed(150);
+      if (greenHeight > 100) {
+//        setMotorSpeed(135);
         followBlock(4);
         direction = 4;
       } else {
         followCenter(4);
       }
     }
-  } else if (direction == 4 && frontDistance < 15) {
+  } else if (direction == 4 && frontDistance < 7 && frontDistance != 0) {
     detectedBlock = false;
     centeredBlock = false;
 
@@ -222,8 +259,8 @@ void loop() {
       int numBlocks = pixy.ccc.numBlocks;
       digitalWrite(in1, LOW);
       digitalWrite(in2, HIGH);
-      setMotorSpeed(100);
-      servo.write(65);
+//      setMotorSpeed(120);
+      servo.write(85);
       for (int i = 0; i < numBlocks; i++) {
         if (pixy.ccc.blocks[i].m_signature == 4) {
           detectedBlock = true;
@@ -240,14 +277,14 @@ void loop() {
         }
       }
       if (greenCenter > 200) {
-        servo.write(75);
+        servo.write(100);
       } else {
-        servo.write(90);
+        servo.write(130);
         centeredBlock = true;
         break;
       }
     }
-  } else if (direction == 1 && frontDistance < 15) {
+  } else if (direction == 1 && frontDistance <= 7 && frontDistance != 0) {
     detectedBlock = false;
     centeredBlock = false;
 
@@ -256,8 +293,8 @@ void loop() {
       int numBlocks = pixy.ccc.numBlocks;
       digitalWrite(in1, LOW);
       digitalWrite(in2, HIGH);
-      setMotorSpeed(100);
-      servo.write(115);
+//      setMotorSpeed(120);
+      servo.write(165);
       for (int i = 0; i < numBlocks; i++) {
         if (pixy.ccc.blocks[i].m_signature == 1) {
           detectedBlock = true;
@@ -274,34 +311,61 @@ void loop() {
         }
       }
       if (redCenter > 120) {
-        servo.write(105);
+        servo.write(145);
       } else {
-        servo.write(90);
+        servo.write(130);
         centeredBlock = true;
         break;
       }
     }
   } else {
-    if (lowSpeed == 1){
-      setMotorSpeed(70);
-    } else {
-      setMotorSpeed(100);
-    }
-    int mappedValue;
+//    if (lowSpeed == 1){
+//      setMotorSpeed(100);
+//    } else {
+//      setMotorSpeed(80);
+//   }
+//    int mappedValue;
     if (leftDistance < 10 && rightDistance < 10) {
-      mappedValue = 90;
+      servoAngle = 130;
     } else if (leftDistance < 5) {
-      mappedValue = 125;
+      servoAngle = 170;
     } else if (rightDistance < 5) {
-      mappedValue = 55;
+      servoAngle = 80;
     } else if (difference < -45) {
-      mappedValue = 125;
+      servoAngle = 170;
     } else if (difference > 45) {
-      mappedValue = 55;
+      servoAngle = 80;
     } else {
-      mappedValue = map(difference, -45, 45, 125, 55);
+      servoAngle = map(difference, -45, 45, 170, 80);
     }
 
-    servo.write(mappedValue);
+    servo.write(servoAngle);
+  
   }
+
+//  int speed;
+//  if (servoAngle > 130) {
+//    speed = map(servoAngle, 130, 170, 90, 110);
+//  } else if (servoAngle <= 130) {
+//    speed = map(servoAngle, 80, 130, 110, 90);
+//  }
+
+  setMotorSpeed(90);
+
+  delay(100);
+
 }
+
+//  if (servoAngle == 0) {
+//    servoSteering = mappedValue;
+//  } else {
+//    servoSteering = servoAngle;
+//  }
+
+//  int speed;
+//  if (servoSteering > 130) {
+//    speed = map(servoSteering, 130, 165, 60, 130);
+//  } else {
+//    speed = map(servoSteering, 85, 130, 130, 60);
+//  }
+//  setMotorSpeed(speed);
